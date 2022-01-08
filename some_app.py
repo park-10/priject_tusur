433c0443f47f58ca2d6be98c42ff33764189fb2a
Андрей Понкратенко <park-10@yandex.ru>
2022-01-08 13:32:15 +0700

    Исправлен фаил some_app_12

--- some_app.py
+++ some_app.py
@@ -9,5 +9,5 @@ def hello():
     return " <html><head></head> <body> Hello World! </body></html>"
 
 
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+# if __name__ == "__main__":
+    # app.run(host='127.0.0.1', port=5000)
