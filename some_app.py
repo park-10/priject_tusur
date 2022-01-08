58692b268bfd71ca5f91532a5b084ec091e8526b
Андрей Понкратенко <park-10@yandex.ru>
2022-01-08 12:52:46 +0700

    Исправлен фаил some_app.

--- some_app.py
+++ some_app.py
@@ -1,1 +1,13 @@
-print("Hello world1")
+from flask import Flask
+
+print("Hello world")
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return " <html><head></head> <body> Hello World! </body></html>"
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
